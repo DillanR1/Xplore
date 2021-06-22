7d291c55c94c623e2919f1d22a1bbd4d5117866c
DillanR1 <dillanroby@yahoo.com>
2021-06-21 18:29:39 -0700

    Front page done, additional pages added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -18,7 +18,27 @@ function Cards() {
             <CardItem
               src="images/mammoth_lakes.jpg"
               text="See the Majestic Eastern Sierras and the town of Mammoth, home of world class skiing and breath-taking nature."
-              label="Adventure"
+              label="Fish Hike Ski "
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/yuba_river.jpg"
+              text="Fish California's Yuba River, home to beautiful trout. The Yuba's terrain varies from rugged outcroppings to large embankments of smooth stone."
+              label="Fish"
+              path="/services"
+            />
+            <CardItem
+              src="images/northstar_snowboard.jpg"
+              text="Outside of Truckee California, Northstar offers slopes for all skill levels. Shred down the mountain, and enjoy a meal by the fire after!"
+              label="ski snowboard"
+              path="/services"
+            />
+            <CardItem
+              src="images/big_pine.jpg"
+              text="Outdoor enthusiasts rejoice! Enjoy camping at Big Pine 15 minutes south of Bishop in the Eastern Sierras."
+              label="Camping Adventure"
               path="/services"
             />
           </ul>
